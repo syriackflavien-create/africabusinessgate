6e1aba340971f8aa91dca158e59325d4db90426a
syriackflavien-create <syriackflavien@gmail.com>
2026-07-23 15:27:43 +0100

    badge

--- src/about/page.tsx
+++ src/about/page.tsx
@@ -6,4 +6,9 @@ export default function AboutPage() {
       </h1>
     </main>
   )
+
+  
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+<div class="badge-base LI-profile-badge" data-locale="fr_FR" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="syriack-flavien-ndamassingba-mbolipatirani-9a7824286" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://cf.linkedin.com/in/syriack-flavien-ndamassingba-mbolipatirani-9a7824286?trk=profile-badge">Syriack Flavien NDAMASSINGBA MBOLIPATIRANI</a></div>
+              
 }
